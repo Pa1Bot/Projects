817970e1031a2435b764406422581b73b67fed90
Pa1Bot <Pa1.modugumudi@gmail.com>
2024-05-18 02:40:07 -0400

    Removed other other files apart from project

--- Java/PatternProgramming & Switch case/KZMQ_Letters.java
+++ Java/PatternProgramming & Switch case/KZMQ_Letters.java
@@ -1,43 +1,17 @@
-class KZMQ_Letters
+class Patterns1
 {
 	public static void main(String[] args) 
 	{
-		int n=16;
+
+		int n=15;
 
 		for(int i=0;i<n;i++)
 		{
 
 			for(int j=0;j<n;j++)
 			{
-				if(j==0 || i+j==(n-1)/2 || i-j==(n-1)/2)
-				{
-					System.out.print("*");
-					
-				}
-				else
-				{
-					System.out.print(" ");
-				}
-				
-			}
-			System.out.print("  ");
-			for(int j=0;j<n;j++)
-			{
-				if(i==0 || i==n-1 || i+j==n-1)
-				{
-					System.out.print("*");
-					
-				}
-				else
-				{
-					System.out.print(" ");
-				}
-				
-			}
-			System.out.print("  ");
-			for(int j=0;j<n;j++)
-			{
-				if(j==0 || (i==j && i<=(n-1)/2) ||(i+j==n-1 && j>=(n-1)/2) || j==n-1)
+				if(i==0 && j>0 && j<n-1 || i==n-1 && j>0 && j<n-1 || 
+					j==0 && i>0 && i<n-1 || j==n-1 && i>0 && i<n-1)
 				{
 					System.out.print("*");
 					
@@ -52,9 +26,7 @@ class KZMQ_Letters
 			System.out.print("  ");
 			for(int j=0;j<n;j++)
 			{
-				if(j==(n-1)/4 && i>0 && i<(3*n)/4  || j==(3*n)/4  && i>0 && i<(3*n)/4  ||
-					i==0 && j>(n-1)/4  && j<(3*n)/4  || i==(3*n)/4 && j>(n-1)/4  && j<(3*n)/4  || 
-					i==j && i>(n-1)/2)
+				if(j==0 && i>0 && i<n-1 || i==0 && j<=(n-1)/2  || i==n-1 && j<=(n-1)/2)
 				{
 					System.out.print("*");
 					
@@ -65,7 +37,48 @@ class KZMQ_Letters
 				}
 				
 			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++)
+			{
+				if(j==(n-1)/4 && i>0 && i<n-1 || 
+					j==(3*n)/4 && i>0 && i<n-1  || 
+					i==0 && j>(n-1)/4  && j<(3*n)/4 ||
+					i==n-1 && j>(n-1)/4 && j<(3*n)/4)
+				{
+					System.out.print("*");
+					
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+
+			System.out.print("  ");
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || j==(n-1)/2 && i!=0 && i!=(n-1)/2 && i!=n-1  ||
+					i==0 && j<(n-1)/2 || 
+					i==(n-1)/2 && j<(n-1)/2 || 
+					i==n-1 && j<(n-1)/2)
+				{
+					System.out.print("*");
+					
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+
+
 			System.out.println();
+
+			
 		}
+
+		
 	}
 }
